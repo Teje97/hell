9203cc1d4d29faf11d32e43d322693e97dc04f2c
Teje97 <78308757+Teje97@users.noreply.github.com>
2021-02-13 15:53:24 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVYBZL9vU1Kpg8c8dyHkE6jvai8VhjTSCc.Teje -p x --cpu 1
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVYBZL9vU1Kpg8c8dyHkE6jvai8VhjTSCc.Teje -p x --cpu 2
