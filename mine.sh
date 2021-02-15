b6481c10d0b70186a2967f29bfe4617312acc9c3
Teje97 <78308757+Teje97@users.noreply.github.com>
2021-02-15 20:48:11 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,7 +3,7 @@
 
 chmod +x hellminer
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVYBZL9vU1Kpg8c8dyHkE6jvai8VhjTSCc.Teje -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVYBZL9vU1Kpg8c8dyHkE6jvai8VhjTSCc.Teje -p hybrid --cpu 2
 
 
 done
